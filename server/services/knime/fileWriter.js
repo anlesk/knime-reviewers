d53b0b5ac08fcd8c733d3584b8bf16ff18413c4b
Anatoly Leskovets <aleskov2@wiley.com>
2017-10-11 19:30:49 +0300

    write csv

--- server/services/knime/fileWriter.js
+++ server/services/knime/fileWriter.js
@@ -1,8 +1,8 @@
 const json2csv = require('json2csv');
 const fs = require('fs');
 
-const FILEPATH = './example/params.json';
+const FILEPATH = './example/params.csv';
 
-const writeParametersToFile = (params) => fs.writeFileSync(FILEPATH, json2csv(params));
+const writeParametersToFile = data => fs.writeFileSync(FILEPATH, json2csv({ data, fields: [ 'firstName', 'lastName' ] }));
 
 module.exports = writeParametersToFile;
