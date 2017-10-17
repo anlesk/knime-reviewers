1a4e0fe8200da06e0cf64affb14dbe02aff7f165
Anatoly Leskovets <aleskov2@wiley.com>
2017-10-17 16:16:16 +0300

    run async knime job

--- server/services/knime/index.js
+++ server/services/knime/index.js
@@ -1,10 +1,11 @@
-const util = require('util');
-const execFile = util.promisify(require('child_process').execFile);
+const spawn = require('child-process-promise').spawn;
 const readCVSFile = require('../../services/knime/fileReader');
 const writeParametersToFile = require('../../services/knime/fileWriter');
 const KnimeException = require('../../exceptions/knime');
 
 let isLocked = false;
+const pathToKnime = 'C:\\Program Files\\KNIME\\knime.exe';
+const pathToWorkflowDir = 'C:\\Users\\Admin\\knime-workspace\\COI checker 1';
 
 const lock = () => {
   if (isLocked) throw new KnimeException('Another job is in progress!', 423);
@@ -17,7 +18,20 @@ const runKnimeJob = async ({ firstName, lastName }) => {
   lock();
   writeParametersToFile({ firstName, lastName });
 
-  const { stdout, error } = await execFile('node', ['--version']);
+  // const { stdout, error } = await execFile('node', ['--version']);
+  const { stdout, error } = await spawn(pathToKnime, [
+    '-consoleLog',
+    '-reset',
+    '-nosave',
+    // '-noexit',
+    '-nosplash',
+    '-application',
+    'org.knime.product.KNIME_BATCH_APPLICATION',
+    `-workflowDir=${pathToWorkflowDir}`,
+    `-workflow.variable=lastName,${lastName},String`,
+    `-workflow.variable=firstName,${firstName},String`
+  ]);
+
   if (error) throw error;
 
   const result = readCVSFile();
